83a5b307f8dfc2513b5463da3f258e653899c02e
as468579 <as468579@gmail.com>
2020-12-04 16:07:16 +0800

    Initial Commit

---
+++ binding_48/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  binding_48
+//
+//  Created by User05 on 2020/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ binding_48/binding_48App.swift
@@ -0,0 +1,17 @@
+//
+//  binding_48App.swift
+//  binding_48
+//
+//  Created by User05 on 2020/12/4.
+//
+
+import SwiftUI
+
+@main
+struct binding_48App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
